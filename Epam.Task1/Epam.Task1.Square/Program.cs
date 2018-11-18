17819bdece94d633a6de0eafab74cded93f3755c
Vlad <qikstr@gmail.com>
2018-11-18 21:12:51 +0400

    Square upd - done

--- Epam.Task1/Epam.Task1.Square/Program.cs
+++ Epam.Task1/Epam.Task1.Square/Program.cs
@@ -10,27 +10,16 @@ namespace Epam.Task1.Square
     {
         public static void PrintSquare(int n)
         {
-            char ch = '*';
-            char[,] squre = new char[n, n]; // crate a two-dimensional array
 
-
-            for (int i = 0; i < n; i++)     // fill the array
+            for (int i = 0; i < n; i++)    
             {
                 for (int j = 0; j < n; j++)
                 {
                     if (j == n / 2 && i == n / 2)
                     {
-                        squre[i, j] = ' ';
+                        Console.Write(' ');
                     }
-                    else squre[i, j] = ch;
-                }
-            }
-
-            for (int i = 0; i < n; i++)     // print the array
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    Console.Write(squre[i, j]);
+                    else Console.Write('*');
                 }
                 Console.WriteLine();
             }
@@ -42,8 +31,7 @@ namespace Epam.Task1.Square
 
             Console.WriteLine("Enter N");
             n = int.Parse(Console.ReadLine());
-            PrintSquare(n);
-            Console.ReadLine();            
+            PrintSquare(n);          
         }
     }
 }
