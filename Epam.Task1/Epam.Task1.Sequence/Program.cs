eec7c414daec8768ed520fbfa9f032e020e7dd9d
Vlad <qikstr@gmail.com>
2018-11-18 19:58:12 +0400

    sequence - done

--- Epam.Task1/Epam.Task1.Sequence/Program.cs
+++ Epam.Task1/Epam.Task1.Sequence/Program.cs
@@ -8,8 +8,22 @@ namespace Epam.Task1.Sequence
 {
     class Program
     {
+
+        public static void PrintSequence (int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.Write(n);
+        }
+
         static void Main(string[] args)
         {
+            int n;
+
+            n = int.Parse(Console.ReadLine()); //read N 
+            PrintSequence(n);    //print Sequence up to N
         }
     }
 }
