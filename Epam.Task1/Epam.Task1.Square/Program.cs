734064d41cba766bf54a5c733267fffa478e1e87
Vladislav-Pushkarev <vlad-pushkarev@mail.ru>
2018-11-18 21:33:05 +0400

    upd

--- Epam.Task1/Epam.Task1.Square/Program.cs
+++ Epam.Task1/Epam.Task1.Square/Program.cs
@@ -12,6 +12,7 @@ namespace Epam.Task1.Square
         {
             for (int i = 0; i < n; i++)    
             {
+
                 for (int j = 0; j < n; j++)
                 {
                     if (j == n / 2 && i == n / 2)
