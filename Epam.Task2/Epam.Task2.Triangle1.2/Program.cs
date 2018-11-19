f1bbbc97ad82f0699770f1725486451d8b5bca2f
Vladislav-Pushkarev <vlad-pushkarev@mail.ru>
2018-11-20 01:19:23 +0400

    Triangle1.2 - done

---
+++ Epam.Task2/Epam.Task2.Triangle1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.Triangle1._2
+{
+    class Program
+    {
+        public static void Triangle(int n)
+        {
+           
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Enter N");
+            n = int.Parse(Console.ReadLine());
+
+            Triangle(n);
+        }
+    }
+}
