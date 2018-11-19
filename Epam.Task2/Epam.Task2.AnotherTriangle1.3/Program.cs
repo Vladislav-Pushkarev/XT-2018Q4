496cb28f5aaf77ca30c62926e7e683706337e47e
Vladislav-Pushkarev <vlad-pushkarev@mail.ru>
2018-11-20 01:53:15 +0400

    enother triangle1.3 - done

---
+++ Epam.Task2/Epam.Task2.AnotherTriangle1.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task2.AnotherTriangle1._3
+{
+    class Program
+    {
+        public static void Triangle(int n)
+        {
+        int numOfSpases = n - 1;
+        int numOfAsterisk = 1;
+          for (int i = 0; i < n; i++)
+          {
+                for (int j = 0; j < numOfSpases; j++)
+                {
+                    Console.Write(' ');
+                }
+                 for (int k= 0; k < numOfAsterisk; k++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+                numOfAsterisk +=2;
+                numOfSpases--;             
+          }
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Enter N");
+            n = int.Parse(Console.ReadLine());
+
+            Triangle(n);
+            Console.ReadLine();
+        }
+    }
+}
