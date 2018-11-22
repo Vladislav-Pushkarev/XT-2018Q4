011f80f11979763cc79600e3c901bfc2c52996f9
Vladislav-Pushkarev <vlad-pushkarev@mail.ru>
2018-11-22 19:50:25 +0400

    ArrayProcessing1.7 - done

--- Epam.Task2/Epam.Task2.ArrayProcessing1.7/Program.cs
+++ Epam.Task2/Epam.Task2.ArrayProcessing1.7/Program.cs
@@ -8,10 +8,10 @@ namespace Epam.Task2.ArrayProcessing1._7
 {
     class Program
     {
-        public static int [] CreateArray (int length, int min, int max)
+        public static int[] CreateArray(int length, int min, int max)
         {
             int[] arr = new int[length];
-           
+
             Random random = new Random();
             for (int i = 0; i < length; i++)
             {
@@ -20,17 +20,58 @@ namespace Epam.Task2.ArrayProcessing1._7
             return arr;
         }
 
-        public static void PrintArray (int[] arr)
+        public static void PrintArray(int[] arr)
         {
+            Console.Write("{ ");
             for (int i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine(arr[i]);
+                Console.Write(arr[i] + " ");
             }
+            Console.WriteLine('}' + Environment.NewLine);
         }
+
+        public static int[] Sort(int[] arr)
+        {
+            int length = arr.Length;
+            int temp = 0;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 1; j < length - i; j++)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        public static int Min(int[] arr)
+        {
+            Sort(arr);
+            return arr[0];
+        }
+
+        public static int Max(int[] arr)
+        {
+            Sort(arr);
+            return arr[arr.Length - 1];
+        }
+
 
         static void Main(string[] args)
         {
-            PrintArray(CreateArray(10, -100, 100));
+            Console.WriteLine("\tArray created :");
+            int[] arr = CreateArray(10, -1000, 1000);
+            PrintArray(arr);
+            Console.WriteLine("The largest number in the array: " + Environment.NewLine + Max(arr));
+            Console.WriteLine("The smallest number in the array: " + Environment.NewLine + Min(arr));
+            Console.WriteLine("\tArray sorted :");
+            PrintArray(arr);
         }
     }
 }
