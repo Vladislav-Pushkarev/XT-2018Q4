aa25bfb89cd24820c6fe8db0db608d817ebc15d2
Vladislav-Pushkarev <vlad-pushkarev@mail.ru>
2018-12-02 14:59:20 +0400

    2.1 - done

---
+++ Epam.Task3/Epam.Task3.Round2.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.Round2._1
+{
+    class Program
+    {
+        public static bool Compare(Round one, Round two)
+        {
+            if (one.Area() > two.Area())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Round round1 = new Round(1, 2, 3);
+            Round round2 = new Round(0, 0, 5);
+
+            try
+            {
+                Round round3 = new Round(0, 0, 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Total created {0} rounds", Round.Count);
+            Console.WriteLine("First round bigger than second? - {0} ", Compare(round1, round2));
+
+        }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.Round2.1/Round.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Round
+{
+    private static int count;
+    private int coordX;
+    private int coordY;
+    private double r;
+    private double lenght;
+    private double area;
+
+
+    public static int Count { get => count; }
+    public int CoordX { get => coordX; set => coordX = value; }
+    public int CoordY { get => coordY; set => coordY = value; }
+    public double R
+    {
+        get
+        {
+            return r;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                r = value;
+            }
+            else
+            {
+                throw new ArgumentException("Radius cannot be negative");
+            }
+        }
+    }
+
+    public Round(int coordX, int coordY, int r)
+    {
+        CoordX = coordX;
+        CoordY = coordY;
+        R = r;
+        count++;
+    }
+
+    public double Lenght()
+    {
+        if (lenght == 0)
+        {
+            lenght = 2 * Math.PI * r;
+        }
+        return lenght;
+    }
+
+    public double Area()
+    {
+        if (area == 0)
+        {
+            area = Math.PI * r * r;
+        }
+        return area;
+    }
+}
