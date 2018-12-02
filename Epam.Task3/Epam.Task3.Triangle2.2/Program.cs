8c47f34042e6dd5e79b9a86bd030d211d272fe89
Vladislav-Pushkarev <vlad-pushkarev@mail.ru>
2018-12-03 02:21:55 +0400

    2.2 - done

---
+++ Epam.Task3/Epam.Task3.Triangle2.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.Triangle2._2
+{
+    public class Program
+    {
+        public static bool Compare(Triangle one, Triangle two)
+        {
+            if (one.Area() > two.Area())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Triangle triangle1 = new Triangle(1, 2, 4);
+            Triangle triangle2 = new Triangle(7.4, 2, 5);
+
+            try
+            {
+                Triangle triangle3 = new Triangle(1, 2, 3);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Total created {0} triangles", Triangle.Count);
+            Console.WriteLine("First triangle bigger than second? - {0} ", Compare(triangle1, triangle2));
+
+            Console.WriteLine("Side A = {0}, Side A = {1},Side A = {2}", triangle2.SideA, triangle2.SideB, triangle2.SideC);
+            try
+            {
+                triangle2.SideC = 6;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Epam.Task3/Epam.Task3.Triangle2.2/Triangle.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task3.Triangle2._2
+{
+    public class Triangle
+    {
+        private static int count;
+        private double sideA;
+        private double sideB;
+        private double sideC;
+        private double perimeter;
+        private double area;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (IsExist(sideA, sideB, sideC))
+            {
+                this.SideA = sideA;
+                this.SideB = sideB;
+                this.SideC = sideC;
+                count++;
+            }
+            else
+            {
+                throw new ArgumentException("One of triangle sides should be bigger than sum of two other.");
+            }
+        }
+
+        public static int Count { get => count; }
+
+        public double SideA
+        {
+            get
+            {
+                return this.sideA;
+            }
+
+            set
+            {
+                if (value > 0 && IsExist(value, this.sideB, this.sideC))
+                {
+                    this.sideA = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Triangle side cannot be negative, " +
+                        "and one of triangle sides should be bigger than sum of other two.");
+                }
+            }
+        }
+
+        public double SideB
+        {
+            get
+            {
+                return this.sideB;
+            }
+
+            set
+            {
+                if (value > 0 && IsExist(this.sideA, value, this.sideC))
+                {
+                    this.sideB = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Triangle side cannot be negative, " +
+                        "and one of triangle sides should be bigger than sum of other two.");
+                }
+            }
+        }
+
+        public double SideC
+        {
+            get
+            {
+                return this.sideC;
+            }
+
+            set
+            {
+                if (value > 0 && IsExist(this.sideA, this.sideB, value))
+                {
+                    this.sideC = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Triangle side cannot be negative, " +
+                        "and one of triangle sides should be bigger than sum of other two.");
+                }
+            }
+        }
+
+        public double Perimeter()
+        {
+            if (this.perimeter == 0)
+            {
+                this.perimeter = this.sideA + this.sideB + this.sideC;
+            }
+
+            return this.perimeter;
+        }
+
+        public double Area()
+        {
+            if (this.area == 0)
+            {
+                double halfP = this.Perimeter() / 2;
+                this.area = Math.Sqrt(halfP * (halfP - this.sideA) * (halfP - this.sideB) * (halfP - this.sideC));
+                return this.area;
+            }
+
+            return this.area;
+        }
+
+        private static bool IsExist(double sideA, double sideB, double sideC)
+        {
+            if (sideA > sideB + sideC || sideB > sideA + sideC || sideC > sideB + sideA)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
