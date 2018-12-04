3ff55d0defa061c88b2c3c260ba3af88b2832a02
Vladislav-Pushkarev <vlad-pushkarev@mail.ru>
2018-12-05 02:21:23 +0400

    MyString done, stylecop check requared

--- Epam.Task3/Epam.Task3.MyString2.4/MyString.cs
+++ Epam.Task3/Epam.Task3.MyString2.4/MyString.cs
@@ -6,9 +6,162 @@ using System.Threading.Tasks;
 
 namespace Epam.Task3.MyString2._4
 {
-    class MyString
+    public class MyString
     {
-        private char[] chars;
+        private char[] myStr;
 
+        public MyString(string input)
+        {
+            this.myStr = input.ToCharArray();
+        }
+
+         public MyString()
+        {
+            this.myStr = null;
+        }
+
+        public MyString(char[] inputCharArr)
+        {
+            this.myStr = inputCharArr;
+        }
+
+        public int Length
+        {
+            get
+            {
+                return this.myStr.Length;
+            }
+        }
+
+        public static MyString operator + (MyString firstStr, MyString secondStr)
+        {
+
+            int lenght = firstStr.myStr.Length + secondStr.myStr.Length;
+            char [] newCharArray = new char[lenght];
+            for (int i = 0; i < lenght; i++) 
+            {
+                for (int j = 0; j < lenght; j++) 
+                {
+                    newCharArray[i] = firstStr.myStr[j];
+                }
+                 for (int k = 0; k < lenght; k++) 
+                {
+                    newCharArray[i] = secondStr.myStr[k];
+                }
+
+            }
+            MyString newMyString = new MyString(newCharArray);
+            return newMyString;
+        }
+
+        public override bool Equals (Object strObj)
+        {
+            if (this == strObj)
+            {
+                return true;
+            }
+            if (strObj is MyString)
+            {
+                MyString anotherString = (MyString)strObj;
+                if (this.myStr.Length == anotherString.myStr.Length)
+                {
+                    for (int i = 0; i < this.Length; i++)
+                    {
+                        if (this.myStr[i] != anotherString.myStr[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator == (MyString firstStr, MyString secondStr)
+        {
+            return firstStr.Equals(secondStr);
+        }
+
+        public static bool operator != (MyString firstStr, MyString secondStr)
+        {
+            return !firstStr.Equals(secondStr);
+        }
+
+        public static int Compare(MyString firstStr, MyString secondStr)
+        {
+            int minLength = firstStr.Length <= secondStr.Length ? firstStr.Length : secondStr.Length;
+            for (int i = 0; i < minLength; i++)
+            {
+                if (firstStr.myStr[i] == secondStr.myStr[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    if (firstStr.myStr[i] < secondStr.myStr[i])
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+            }
+
+            if (firstStr.Length == secondStr.Length)
+            {
+                return 0;
+            }
+            else
+            {
+                if (firstStr.Length < secondStr.Length)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return new string(this.myStr);
+        }
+
+        public int IndexOf(char ch)
+        {
+            for (int i = 0; i < this.Length; i++)
+            {
+                if (this.myStr[i] == ch)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public char[] ToCharArray()
+        {
+            return this.myStr;
+        }
+        
+        public char CharAt(int index)
+        {
+            if (index < 0 || index >= this.myStr.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return this.myStr[index];
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
--- Epam.Task3/Epam.Task3.MyString2.4/Program.cs
+++ Epam.Task3/Epam.Task3.MyString2.4/Program.cs
@@ -10,6 +10,33 @@ namespace Epam.Task3.MyString2._4
     {
         static void Main(string[] args)
         {
+            string one = "It’s better to be safe than sorry.";
+            string oneCopy = "It’s better to be safe than sorry.";
+            string two = "People who live in glass houses should not throw stones.";
+
+            MyString myOne = new MyString(one);
+            MyString myOneCopy = new MyString(oneCopy);
+            MyString myTwo = new MyString(two);
+
+            Console.WriteLine("\tEquals method demonstration:");
+            Console.WriteLine(myOne.Equals(myOneCopy));
+            Console.WriteLine(myOne.Equals(myOne));
+            Console.WriteLine(myOne.Equals(two));
+            Console.WriteLine("\t== and != operators demonstration:");
+            Console.WriteLine(myOne == myOneCopy);
+            Console.WriteLine(myOne != myOne);
+            Console.WriteLine(myOne != myTwo);
+
+            char[] charArr = myOne.ToCharArray();
+            Console.WriteLine("\tIndexOf and CharAt demonstration:");
+            Console.WriteLine(myOne.IndexOf('s'));
+            Console.WriteLine(myOne.CharAt(3));
+            Console.WriteLine("\tCompare demonstration:");
+            Console.WriteLine(MyString.Compare(myOne, myOneCopy));
+            Console.WriteLine(MyString.Compare(myOne, myTwo));
+            Console.WriteLine(MyString.Compare(myTwo, myOne));
+
+            Console.ReadLine();
         }
     }
 }
