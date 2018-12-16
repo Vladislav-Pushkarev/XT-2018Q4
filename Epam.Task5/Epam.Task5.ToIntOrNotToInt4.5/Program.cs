5cff98985c6de9b4e2fd0b5ac0a807c052a75d6b
Vladislav-Pushkarev <vlad-pushkarev@mail.ru>
2018-12-16 04:33:20 +0400

    4.5 - done

---
+++ Epam.Task5/Epam.Task5.ToIntOrNotToInt4.5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task5.ToIntOrNotToInt4._5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str1 = "7,04E+21";
+            string str2 = "31,4E+01";
+            string str3 = "6,02214129E23";
+            string str4 = "394 e-03";
+            string str5 = "+3r944e-03";
+            string str6 = "-323";
+            string str7 = "323-4324";
+            string str8 = "323,43,24";
+            Console.WriteLine("\tDemonstration:");
+            Console.WriteLine($"Is there - \"{str1}\" positive integer number? - {str1.ToInt()} ");
+            Console.WriteLine($"Is there - \"{str2}\" positive integer number? - {str2.ToInt()} ");
+            Console.WriteLine($"Is there - \"{str3}\" positive integer number? - {str3.ToInt()} ");
+            Console.WriteLine($"Is there - \"{str4}\" positive integer number? - {str4.ToInt()} ");
+            Console.WriteLine($"Is there - \"{str5}\" positive integer number? - {str5.ToInt()} ");
+            Console.WriteLine($"Is there - \"{str6}\" positive integer number? - {str6.ToInt()} ");
+            Console.WriteLine($"Is there - \"{str7}\" positive integer number? - {str7.ToInt()} ");
+            Console.WriteLine($"Is there - \"{str8}\" positive integer number? - {str8.ToInt()} ");
+
+            while (true)
+            {
+                Console.WriteLine("\tEnter 'q' - to quit or ==>\tTry your own number:");
+                string str9 = Console.ReadLine();
+                if (str9 == "q")
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Is there - \"{str8}\" positive integer number? - {str9.ToInt()} ");
+            }
+        }
+    }
+}
---
+++ Epam.Task5/Epam.Task5.ToIntOrNotToInt4.5/StringExtension.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task5.ToIntOrNotToInt4._5
+{
+    public static class StringExtension
+    {
+        public static bool ToInt(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+            bool exp = false;
+            bool com = false;
+            bool minus = false;
+
+            int i = 0;
+            int length = str.Length;
+            List<char> integer = new List<char>();
+            List<char> decim = new List<char>();
+            List<char> pwr = new List<char>();
+            char firstChar = str[0];
+
+            if (firstChar == '+')
+            {
+                i++;
+            }
+
+            if (length == 1)
+            {
+                return false;
+            }
+
+            if (!char.IsDigit(str[i]) && str[i] == '0')
+            {
+                return false;
+            }
+
+            for (; i < length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    if (exp)
+                    {
+                        pwr.Add(str[i]);
+                        continue;
+                    }
+
+                    if (com)
+                    {
+                        decim.Add(str[i]);
+                        continue;
+                    }
+
+                    if (!com && !exp)
+                    {
+                        integer.Add(str[i]);
+                    }
+                }
+                else if (str[i] == ',')
+                {
+                    if (com)
+                    {
+                        return false;
+                    }
+
+                    com = true;
+                    if (i == length - 1 || !char.IsDigit(str[i + 1]))
+                    {
+                        return false;
+                    }
+                }
+                else if (char.ToLower(str[i]) == 'e')
+                {
+                    if (exp)
+                    {
+                        return false;
+                    }
+
+                    exp = true;
+                    if (i + 1 < length - 1)
+                    {
+                        i++;
+
+                        if (str[i] == '-' || str[i] == '+')
+                        {
+                            if (str[i] == '-')
+                            {
+                                minus = true;
+                            }
+                        }
+                        else
+                        {
+                            i--;
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            int power = 0;
+            i = 0;
+
+            foreach (char ch in pwr)
+            {
+                power = (power * 10) + ((int)char.GetNumericValue(ch));
+            }
+
+            if (minus)
+            {
+                power = power * -1;
+            }
+
+            if ((power + integer.Count) > decim.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
